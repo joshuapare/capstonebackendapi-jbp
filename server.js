8b3639ac658a45b049e86567db9ffd4e8777829a
Joshua Pare <65102852+sbhrule15@users.noreply.github.com>
2020-11-28 09:41:15 -0500

    fixingdotenv

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
-require('dotenv').config();
 
 app.use(cors());
 app.use(bodyParser.json());
